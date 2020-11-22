4211158ad95d441a94c387ed0e6cceb754f1d3fb
Dines Dines <dines9748@gmail.com>
2020-11-23 03:56:16 +0530

    sample

---
+++ Vidly/Migrations/202011201810248_updateMembershipTypeName.cs
@@ -0,0 +1,18 @@
+﻿namespace Vidly.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updateMembershipTypeName : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("UPDATE MembershipTypes SET Name = 'Pay as You Go' WHERE Id=1");
+            Sql("UPDATE MembershipTypes SET Name = 'Monthly' WHERE Id = 2");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
